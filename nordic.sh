c089a2df25e53d4de8b56d98a4417a51116738ef
Davi-Barbado <davibarbadodossantos@gmail.com>
2026-03-04 11:28:47 -0300

    Update nordic.sh

--- nordic.sh
+++ nordic.sh
@@ -2,5 +2,6 @@ git clone https://github.com/EliverLara/Nordic.git
 mkdir -p ~/.themes
 mv Nordic ~/.themes
 cd ~/dotfiles-davibarbado
+git clone https://github.com/yeyushengfan258/Twilight-Cursors.git
 mkdir -p ~/.icons
 cp -r Twilight-cursors ~/.icons
