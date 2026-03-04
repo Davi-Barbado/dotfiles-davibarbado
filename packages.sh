00ce5bf70293e42e5dc442a3c227cdc81bcdbbf5
Davi-Barbado <davibarbadodossantos@gmail.com>
2026-03-04 10:03:32 -0300

    Create packages.sh

---
+++ packages.sh
@@ -0,0 +1,5 @@
+sudo pacman -Syu --noconfirm --needed kitty rofi nemo nemo-fileroller nwg-look wget tar scrot picom autotiling fastfetch feh git base-devel
+yay -S --noconfirm --needed brave-bin volctl
+git clone https://github.com/jluttine/rofi-power-menu.git
+cd rofi-power-menu
+sudo cp rofi-power-menu /usr/bin
