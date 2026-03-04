604be69f81e87ad03d11aea254fba28d737c93ac
Davi-Barbado <davibarbadodossantos@gmail.com>
2026-03-04 10:27:19 -0300

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,4 +1,4 @@
-sudo pacman -Syu --noconfirm --needed kitty rofi nemo nemo-fileroller nwg-look wget tar xfce4-screenshooter picom autotiling fastfetch feh git base-devel
+sudo pacman -Syu --noconfirm --needed ttf-jetbrains-mono-nerd starship kitty rofi nemo nemo-fileroller nwg-look wget tar xfce4-screenshooter picom autotiling fastfetch feh git base-devel
 yay -S --noconfirm --needed brave-bin volctl
 git clone https://github.com/jluttine/rofi-power-menu.git
 cd rofi-power-menu
